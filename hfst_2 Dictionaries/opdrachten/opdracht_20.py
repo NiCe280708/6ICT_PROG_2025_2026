491a282431821ac271e114753467aa6b40a36981
NiCe280708 <Cedricninh28@gmail.com>
2025-10-07 09:44:47 +0200

    smley

--- hfst_2 Dictionaries/opdrachten/opdracht_20.py
+++ hfst_2 Dictionaries/opdrachten/opdracht_20.py
@@ -4,3 +4,17 @@ smiley = [
     [3, 0, 0, 0, 3],
     [0, 3, 3, 3, 0]
 ]
+woord_som = ""
+teller = 0
+for index, lijsten in enumerate(smiley):
+    woord_som = ""
+    for nummers in smiley[index]:
+        if nummers == 0:
+            woord_som += " "
+        elif nummers == 1:
+            woord_som += "#"
+        elif nummers == 2:
+            woord_som += "."
+        elif nummers == 3:
+            woord_som += "-"
+    print (woord_som)
